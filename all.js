e40343f1500d13dab1c2534bcfb794ce5c3d8ec4
yellowHuang <hlady1115@gmail.com>
2020-04-28 20:26:50 +0800

    add: 新增執行 把 callback 丟給 watcher 讓 watcher 決定什麼時候要執行

--- all.js
+++ all.js
@@ -2,58 +2,55 @@
  *  通用
  *  */
 // 原始資料
-const origin = { first: 'Yellow', last: 'Huang' }
+const origin = { first: 'Yellow', last: 'Huang' };
 // 檢查 fun
 const isObject = function (obj) {
-    return obj && typeof obj === 'object'
-}
+  return obj && typeof obj === 'object';
+};
 
 /**
- *  觀察者模式
+ *  觀察者模式 封裝原始資料
  *  */
-// 封裝 fun
-// const defineProperty = function (data, key) {
-//     let value = data[key]
-//     // 覆蓋原本資料
-//     Object.defineProperty(data, key, {
-//         get() {
-//             console.log('ggggget: ', key)
-//             return value
-//         },
-//         set(val) {
-//             console.log('ssssset: ', key)
-//             value = val
-//         },
-//     })
-//     // 再檢查內層是否為物件
-//     if (isObject(value)) observe(value)
-// }
 const observe = function (data) {
-    if (isObject(data)) {
-        // 針對每個 key 做封裝
-        Object.keys(data).forEach((key) => {
-            let value = data[key]
-            // 使用 defineProperty 進行覆蓋原本資料
-            Object.defineProperty(data, key, {
-                get() {
-                    console.log('ggggget: ', key)
-                    return value
-                },
-                set(val) {
-                    console.log('ssssset: ', key)
-                    value = val
-                },
-            })
-            // 再檢查內層是否為物件
-            if (isObject(value)) observe(value)
-        })
-    }
-    return data
-}
-const data = observe(origin)
+  if (isObject(data)) {
+    // 針對每個 key 做封裝
+    Object.keys(data).forEach((key) => {
+      let value = data[key];
+      // 使用 defineProperty 進行覆蓋原本資料
+      Object.defineProperty(data, key, {
+        get() {
+          console.log('ggggget: ', key);
+          return value;
+        },
+        set(val) {
+          console.log('ssssset: ', key);
+          value = val;
+        },
+      });
+      // 再檢查內層是否為物件
+      if (isObject(value)) observe(value);
+    });
+  }
+  return data;
+};
 
-function render() {
-    document.querySelector('h1').innerHTML = `My name is ${data.first} ${data.last}`
-}
+// 取用的 data 是已經經過封裝後的資料
+const data = observe(origin);
 
-render()
+// 註冊依賴 把 callback 丟給 watcher 讓 watcher 決定什麼時候要執行
+const watcher = function (callback) {
+  this.callback = callback;
+  this.render();
+};
+
+watcher.prototype.render = function () {
+  this.callback();
+};
+
+new watcher(() => {
+  document.querySelector('h1').innerHTML = `My name is ${data.first} ${data.last}`;
+});
+
+// function render() {
+//   document.querySelector('h1').innerHTML = `My name is ${data.first} ${data.last}`;
+// }
